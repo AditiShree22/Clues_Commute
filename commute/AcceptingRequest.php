3111f7f34cb94cfc0f6ddd33126a94b49e2b0a27
AditiShree22 <33476300+AditiShree22@users.noreply.github.com>
2017-11-23 11:41:08 +0530

    Update AcceptingRequest.php

--- commute/AcceptingRequest.php
+++ commute/AcceptingRequest.php
@@ -5,54 +5,67 @@ $db = new DB_Functions();
  
 // json response array
 $response = array("error" => FALSE);
+
+$jsonArr=json_decode(file_get_contents('php://input'),true);
  
-if (isset($_GET['acceptor_id']) && isset($_GET['sender_id'])) {
+if (isset($jsonArr['acceptor_id']) && isset($jsonArr['sender_id'])) {
  
     // receiving the post params
-    $acceptor_id= $_GET['acceptor_id'];
-    $sender_id = $_GET['sender_id'];
+
+    $acceptor_id = $jsonArr['acceptor_id'];
+    $sender_id = $jsonArr['sender_id'];
 
     $type = $db->fetchFromUsersAccessType($acceptor_id);
 
     if($type === "driver"){
         if ($db->updateStatus($acceptor_id, $sender_id, 1)) {
         // user already existed
-        $response["response"] = TRUE;
+        $response["status"] = "Success";
+        $response["code"] = "200";
+        $response["message"] = "Request Accepted"; 
+        $response["error"] = (object)array();
         echo json_encode($response);
     } 
 
     else {
             // user failed to store
-            $response["error"] = TRUE;
-            $response["error_msg"] = "Updation Unsuccessful!";
+            $response["status"] = "Failed";
+            $response["error"]["error_code"] = "304" ;
+            $response["error"]["error_msg"] = "Updation Unsuccessful!";
             echo json_encode($response);
         }
     }
     elseif ($type === "passenger") {
-        if ($db->updateStatus($sender_id, $acceptor_id, 1)) {
+       // if ($db->updateStatus($sender_id, $acceptor_id, 1)) {
         // user already existed
-        $response["response"] = TRUE;
+        $response["status"] = "Forbidden !";
+        $response["code"] = "403";
+        $response["message"] = "As a passenger you cannot accept requests !"; 
+        $response["error"] = (object)array();
         echo json_encode($response);
-    } 
+    /*} 
 
     else {
             // user failed to store
-            $response["error"] = TRUE;
-            $response["error_msg"] = "Updation Unsuccessful!";
+            $response["status"] = "Failed";
+            $response["error"]["error_code"] = "204" ;
+            $response["error"]["error_msg"] = "Updation Unsuccessful!";
             echo json_encode($response);
-        }
+        }*/
     }
 
     else{
-        $response["error"] = TRUE;
-        $response["error_msg"] = "Could not get access type of acceptor!";
+        $response["status"] = "Failed";
+        $response["error"]["error_code"] = "412" ;
+        $response["error"]["error_msg"] = "Could not get access type of acceptor!";
         echo json_encode($response);
     }
 
     }
 else {
-    $response["error"] = TRUE;
-    $response["error_msg"] = "Required parameters (driver_id or passenger_id) is missing!";
+    $response["status"] = "Failed";
+    $response["error"]["error_code"] = "400" ;
+    $response["error"]["error_msg"] = "Required parameters (driver_id or passenger_id) is missing!";
     echo json_encode($response);
 }
 ?>
