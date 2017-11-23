e710a5805fece4bed1e271d2dc5f720f52fe2225
AditiShree22 <33476300+AditiShree22@users.noreply.github.com>
2017-11-23 11:43:23 +0530

    Update ViewReceivedRequests.php

--- commute/ViewReceivedRequests.php
+++ commute/ViewReceivedRequests.php
@@ -2,10 +2,11 @@
  
 require_once 'include/DB_Functions.php';
 $db = new DB_Functions();
+
 $response = array("error" => FALSE);
  
 // json response array 
-if (isset($_GET['employee_id'])) {
+if (isset($_GET['employee_id']) && !empty($_GET['employee_id'])) {
  
     // receiving the post params
     $employee_id= $_GET['employee_id'];
@@ -17,24 +18,33 @@ if (isset($_GET['employee_id'])) {
 
     //print_r($result);
     if($result){
+             $response["status"] = "Success";
+             $response["code"] = "200";
+             $response["message"] = "Received Request"; 
+             $response["error"] = (object)array();
         for ($i=0; $i<count($result); $i++)
         {
 
-            $response["response"][$i] = array("employee_id"=>$result[$i]['employee_id'],"employee_name"=>$result[$i]['employee_name'],"address"=>$result[$i]['address'],"mobile_number"=>$result[$i]['mobile_number'],"latitude"=>$result[$i]['latitude'], "longitude"=>$result[$i]['longitude']);
+            $response["response"][$i] = array("employee_id"=>$result[$i]['employee_id'],"employee_name"=>$result[$i]['employee_name'],"email"=>$result[$i]['email'],"mobile_number"=>$result[$i]['mobile_number'],"gender"=>$result[$i]['gender']);
             
 
         }
     echo json_encode($response);
     }
     else{
-        $response["error"] = TRUE;
-            $response["error_msg"] = "No Request Received";
+         $response["status"] = "Failed";
+         $response["error"]["error_code"] = "204" ;
+         $response["error"]["error_msg"] = "No request received !";
+         $response["response"] = (object)array();
             echo json_encode($response);
-    }
+        }
     }
 
     else{
-        $response["error"] = TRUE;
+    $response["status"] = "Failed";
+    $response["error"]["error_code"] = "400" ;
+    $response["error"]["error_msg"] = "Bad request : Required parameter is missing!";
+    $response["response"] = (object)array();
     $response["error_msg"] = "Required parameter (employee_id) is missing!";
     echo json_encode($response);
     }
