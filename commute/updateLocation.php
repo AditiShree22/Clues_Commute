caea746784c1912521b60dd1cacc7ce4d6072030
AditiShree22 <33476300+AditiShree22@users.noreply.github.com>
2017-12-05 11:57:59 +0530

    Update updateLocation.php

--- commute/updateLocation.php
+++ commute/updateLocation.php
@@ -8,26 +8,33 @@
 	 
 	 if (isset($_GET['driver_id'])&& isset($_GET['current_lat'])&& isset($_GET['current_long']))
 	 {
-	    	$driver_id = $_GET['driver_id'];
+	    $driver_id = $_GET['driver_id'];
 		$current_lat = $_GET['current_lat'];
 		$current_long = $_GET['current_long'];
 		
 		if ($db->updateLatLong($driver_id, $current_lat, $current_long))
-		{
-			$response["response"] = "Location updated";
+		{  
+		    $response["error"] = (object)array();
+            $response["status"] = "Success";
+            $response["code"] = "200";
+			$response["message"] = "Location updated";
 			echo json_encode($response);
 		}
 		else {
         	// user is not found with the given credentials
-			$response["error"] = TRUE;
-			$response["error_msg"] = "One or more parameters is not correct. Please try again!";
+        	$response["status"] = "Failed";
+			$response["error"]["error_code"] = "406";
+			$response["error"]["error_msg"] = "One or more parameters is not correct. Please try again!";
+			$response["user"] = (object)array();
 			echo json_encode($response);
   	  }
 	}
 	else {
     //required  params is missing
-	    $response["error"] = TRUE;
-	    $response["error_msg"] = "Required parameters are missing!";
+		$response["status"] = "Failed";
+	    $response["error_code"]["error"] = "400";
+	    $response["error"]["error_msg"] = "Required parameters are missing!";
+	    $response["user"] = (object)array();
 	    echo json_encode($response);
             }
 ?>
